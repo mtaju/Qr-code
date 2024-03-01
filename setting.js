512bd8898cce63cef43d181d57af9627a6391855
mtaju <lazacklazaro7@gmail.com>
2024-03-01 12:22:07 +0300

    Create setting.js

---
+++ setting.js
@@ -0,0 +1,8 @@
+const chalk = require("chalk")
+const fs = require("fs")
+
+global.autoTyping = true
+global.autoRecord = false
+global.dcai = true
+
+miftahapi = "zex" // https://api.miftahganzz.my.id
